fbf3598bf23339fc8fb229b5e23bcd8c1691bf60
hillmanpick <1019482178@qq.com>
2024-04-10 17:02:32 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
     before_action :authenticate_user! , only: [:new,:create,:edit,:update,:destroy]
-    before_action :find_group_and_check_permission, only :[:edit,:update,:destory]
+   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups =Group.all
   end
@@ -38,7 +38,7 @@ end
   end
 
 def destroy
-  
+
   @group.destroy
   flash[:alert] = "Group deleted"
   redirect_to groups_path
@@ -47,13 +47,12 @@ end
 
     private
 
-def find_group_and_check_permission
-  @group = Group.find(params[:id])
-  if current_user ! =@group.user
-    redirect_to root_path,alert:"You have no permission."
-  end
-end
-
+     def find_group_and_check_permission
+        @group = Group.find(params[:id])
+        if current_user != @group.user
+          redirect_to root_path, alert: "You have no permission."
+        end
+      end
     def group_params
       params.require(:group).permit(:title,:description)
     end
