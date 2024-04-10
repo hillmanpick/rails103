16b80ef7aa8717b720fb2fc18feed5eeb8bf1b44
hillmanpick <1019482178@qq.com>
2024-04-10 19:07:03 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
+    menber do
+      post :join
+      post :quit
+    end 
     resources :posts
+
   end
   root 'groups#index'
 end
